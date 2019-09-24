972f8344b3f9df5ed96b980781971a087db960aa
eGarciaAlamilla97 <edgarciaalamilla@davidson.edu>
2019-09-24 02:55:52 -0400

    implement header_parse() functionality

--- http.c
+++ http.c
@@ -19,23 +19,47 @@ int header_complete(char *buffer, int buffer_length) {
 }
 
 int header_parse(char *buffer, int buffer_length, char *filename, int filename_length, char *protocol, int protocol_length, int *content_length) {
-	if(!buffer || !filename || !protocol || !content_length) {
-		fprintf(stderr, "Please provide non-null buffer/filename/protocol/content-length\n");
+	// if(!buffer || !filename || !protocol || !content_length) {
+	// 	fprintf(stderr, "Please provide non-null buffer/filename/protocol/content-length\n");
 
-		return -1;
-	}
+	// 	return -1;
+	// }
 
-	if(buffer_length < 128 || filename_length < 128 || protocol_length < 16) {
-		fprintf(stderr, "Please provide buffer/filename strings with sizes >= 128 and a protocol string with size >= 16\n");
+	// if(buffer_length < 128 || filename_length < 128 || protocol_length < 16) {
+	// 	fprintf(stderr, "Please provide buffer/filename strings with sizes >= 128 and a protocol string with size >= 16\n");
 
-		return -1;
-	}
+	// 	return -1;
+	// }
+
+	char* rest;
 
 	buffer[buffer_length] = '\0';
 
-	// TODO: Your code here
-	// Starting by finding the "GET /" or "POST /" string using strcasestr()
-	// There's a small program called parsing.c that demonstrates the usage of strcasestr() in this directory
+	filename = strcasestr(buffer, "/") + 1;
+	filename_length = strlen(filename);	//are we given filename_length?
+
+	rest = strcasestr(filename, " ");
+	*rest = '\0';
+	rest++;
+
+	protocol = strcasestr(rest, "HTTP");
+	protocol_length = strlen(protocol);
+
+	rest = strcasestr(protocol, " ");
+	*rest = '\0';
+	rest++;
+
+	//extending for part II
+	// char* host = strcasestr(rest, "Host:");
+	// int host_length = strlen(host);
+
+	// rest = strcasestr(host, " ");
+	// *rest = '\0';
+	// rest++;
+
+	printf("%s\n", filename);
+	printf("%s\n", protocol);
+	// printf("%s\n", host);
 
 	return -1;
 }
@@ -52,4 +76,17 @@ void fill_reply_403(char *buffer, char *filename, char *protocol) {
 }
 
 void fill_reply_404(char *buffer, char *filename, char *protocol) {
+}
+
+
+int main(){
+	//for testing
+	char header[128] = "PUT /filename.txt HTTP/1.1 Host: localhost:9000";
+	int buffer_length;
+	char* filename;
+	int filename_length;
+	char* protocol;
+	int protocol_length;
+	int* content_length;
+	header_parse(header, 128, filename, filename_length, protocol, protocol_length, content_length);
 }
